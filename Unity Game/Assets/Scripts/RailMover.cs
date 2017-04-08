8c49c214f9f418b66d6b787f9cf925223838a980
Dan Sarver <lostmtfarm@gmail.com>
2017-04-08 14:35:06 -0400

    Updated RailMover.cs

--- Unity Game/Assets/Scripts/RailMover.cs
+++ Unity Game/Assets/Scripts/RailMover.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RailMover : MonoBehaviour {
+public class RailMover : MonoBehaviour
+{
+    private Rail rail;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private int currentSeg;
+    private float transition;
+    private bool isCompleted;
+
+    private void Update()
+    {
+        if (!rail)
+            return;
+
+        if (!isCompleted)
+            Play();
+    }
+
+    private void Play()
+    {
+        transition += Time.deltaTime * 1 / 2.5f;
+        if (transition > 1)
+        {
+            transition = 0;
+            currentSeg++;
+        }
+        else if (transition < 0)
+        {
+            transition = 1;
+            currentSeg--;
+        }
+
+        transform.position = rail.LinearPosition(currentSeg, transition);
+        transform.rotation = rail.Orientation(currentSeg, transition);
+
+    }
+
 }
