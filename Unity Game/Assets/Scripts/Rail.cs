f0011562953ffee07f9d905bb255ad4ea9838581
Dan Sarver <lostmtfarm@gmail.com>
2017-04-08 14:55:18 -0400

    Updated Rail.cs and RailMover.cs

--- Unity Game/Assets/Scripts/Rail.cs
+++ Unity Game/Assets/Scripts/Rail.cs
@@ -20,7 +20,34 @@ public class Rail : MonoBehaviour {
 
 		return Vector3.Lerp(p1,p2,ratio);
 	}
+    public Vector3 CatmullPosition(intseg, float ratio)
+    {
+        Vector3 p1, p2, p3, p4;
 
+        if (seg == 0)
+        {
+            p1 = nodes[seg].position;
+            p2 = p1;
+            p3 = nodes[seg + 1].position;
+            p4 = nodes[seg = 2].position;
+        }
+        else if(seg == nodes.Length - 2)
+        {
+            p1 = nodes[seg - 1].position;
+            p2 = nodes[seg].position;
+            p3 = nodes[seg + 1].position;
+            p4 = p3;
+        }
+        else
+        {
+            p1 = nodes[seg - 1].position;
+            p2 = nodes[seg].position;
+            p3 = nodes[seg + 1].position;
+            p4 = nodes[seg + 2].position;
+        }
+
+
+    }
 	public Quaternion Orientation(int seg, float ratio)
 	{
 		Quaternion q1 = nodes [seg].rotation;
