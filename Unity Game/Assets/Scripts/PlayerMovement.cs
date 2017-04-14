85bf3b676419c9fcdd9898c2b972d385fa2d0e9b
Dan Sarver <lostmtfarm@gmail.com>
2017-04-14 12:17:35 -0400

    Added Gamepad controler toggle
    "G" key

--- Unity Game/Assets/Scripts/PlayerMovement.cs
+++ Unity Game/Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,9 @@ public class PlayerMovement : MonoBehaviour
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
-        
+
+    private bool isInDirectMode = false; // TODO Consider making this static later
+       
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
@@ -22,6 +24,40 @@ public class PlayerMovement : MonoBehaviour
 
     // Fixed update is called in sync with physics
     private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.G)) // TODO add to menu later. G for gamepad. 
+        {
+            print ("G toggler");
+
+            isInDirectMode = !isInDirectMode; //toggle mode
+        }
+
+        if (isInDirectMode)
+        {
+            // ProcessDirectMovement
+        }
+        else
+        {
+            ProcessMouseMovement();
+        }
+        
+
+    }
+
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // calculate camera relative direction to move:
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+        m_Character.Move(m_Move, false, false);
+    }
+
+
+    private void ProcessMouseMovement()
     {
         if (Input.GetMouseButton(0))
         {
@@ -66,7 +102,6 @@ public class PlayerMovement : MonoBehaviour
         {
             m_Character.Move(Vector3.zero, false, false);
         }
-
     }
 }
 
